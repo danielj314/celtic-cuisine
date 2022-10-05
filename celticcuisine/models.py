02ea681d44ddd39084ddd59961d392c40ab15a7c
danielj314 <danieljones314@gmail.com>
2022-10-05 15:57:35 +0000

    changed uri to connect with DATABASE_URL and created database in postgres

--- celticcuisine/models.py
+++ celticcuisine/models.py
@@ -1,4 +1,4 @@
-from celticcuisine import db, mongo
+from celticcuisine import db
 
 
 class Nations(db.Model):
@@ -14,7 +14,7 @@ class Nations(db.Model):
 class Users(db.Model):
     # schema for the Users model
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
 
     def __repr__(self):
--- celticcuisine/routes.py
+++ celticcuisine/routes.py
@@ -1,11 +1,17 @@
 from flask import (
-    Flask, flash, render_template,
+    flash, render_template,
     redirect, request, session, url_for)
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 from celticcuisine import app, db, mongo
-from celticcuisine.models import Nations
+from celticcuisine.models import Nations, Users
 
 
 @app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/get_recipes")
 def get_recipes():
     recipes = list(mongo.db.recipes.find())
